2415de07ca9517141d9b7e39056075ed6f556a9b
Aman-Thewhiz <amanranju23@gmail.com>
2026-04-30 22:59:40 +0530

    fix cors properly

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,10 @@ const { getCorsOrigin } = require("./config/env");
 const app = express();
 
 const corsOptions = {
-  origin: getCorsOrigin,
+  origin: [
+    "http://localhost:5173",
+    "https://taskflow-teal-tau.vercel.app"
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
